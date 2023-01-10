a6e6251d9e023fcda2778c98c5addccbcc6f6e98
seeoya <seeoya@naver.com>
2023-01-10 15:52:57 +0900

    메뉴 변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,18 @@ export default function Header() {
         <header className="text-gray-600 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-                    <Link href="/" className="mr-5 hover:text-gray-900">First Link</Link>
-                    <Link href="/list" className="mr-5 hover:text-gray-900">Second Link</Link>
-                    <Link href="/item/asdsad" className="mr-5 hover:text-gray-900">Third Link</Link>
-                    <Link href="/img" className="hover:text-gray-900">Fourth Link</Link>
+                    <Link href="/" className="mr-5 hover:text-gray-900">
+                        HOME
+                    </Link>
+                    <Link href="/list" className="mr-5 hover:text-gray-900">
+                        LIST
+                    </Link>
+                    <Link href="/item/item1" className="mr-5 hover:text-gray-900">
+                        ITEM
+                    </Link>
+                    <Link href="/img" className="hover:text-gray-900">
+                        IMG
+                    </Link>
                 </nav>
                 <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
