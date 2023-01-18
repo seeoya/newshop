9296b7ba4181f6cdf79c910f19bac52993734816
seeoya <seeoya@naver.com>
2023-01-18 18:08:44 +0900

    _document 추가, SEO 테스트

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Head, Html, Main, NextScript } from "next/document";
+import Router, { useRouter } from "next/router";
+
+// _app.tsx > 본문 페이지 > _document.tsx 순서로 렌더링됨
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <title>가나다라</title>
+                <meta name="description" content="newshop desc" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
--- pages/img.tsx
+++ pages/img.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Image from "next/image";
 
 export default function ImagePage() {
@@ -8,14 +9,21 @@ export default function ImagePage() {
     }
 
     return (
-        <div className="flex flex-wrap">
-            {imgList.map((item, index) => {
-                return (
-                    <span key={index} className="relative w-[100px] h-[300px]">
-                        <Image key={index} src={item} layout="fill" quality={100} alt="" className="object-cover" />
-                    </span>
-                );
-            })}
-        </div>
+        <>
+            <Head>
+                <title>이미지이이이</title>
+                <meta name="description" content="abcdefg img" />
+            </Head>
+
+            <div className="flex flex-wrap">
+                {imgList.map((item, index) => {
+                    return (
+                        <span key={index} className="relative w-[100px] h-[300px]">
+                            <Image key={index} src={item} layout="fill" quality={100} alt="" className="object-cover" />
+                        </span>
+                    );
+                })}
+            </div>
+        </>
     );
 }
